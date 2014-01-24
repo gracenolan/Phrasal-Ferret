833d6806614f84fb549bdb576424fa039ae46486
Grace Nolan <grace.m.nolan@gmail.com>
2014-01-24 16:30:21 +1300

    reading xml
    so I have managed to get it to read the xml docs, but there was some
    problems where the program would crash when facing an unknown
    character.
    
    I have tried to fix this using codecs module. But this hasn’t fixed the
    problem yet.
    
    committing before I make more changes

--- FirstFerret.py
+++ FirstFerret.py
@@ -1,18 +1,29 @@
 #! /usr/bin/python
 
+import codecs
 import xml.etree.ElementTree as ET
 import sys
+import fnmatch
 
 # Read in the file name
-filename = sys.stdin.readlines()
-if filename != "*.xml" :
-  print "Phrasel Ferret likes to climb xml trees only. They have the softest leaves and pleasant smelling flowers. Please try again with an xml file >^_^<"
-else if filename == null:
-  print "Usage: FirstFerret.py <xml file> "
-else tree = ET.parse(filename) # build tree 
-root = tree.getroot()
+if len(sys.argv) != 2:
+  print "Usage: FirstFerret.py <xml file> \nPhrasel Ferret likes to climb xml trees. They have the softest leaves and pleasant smelling flowers. Please try again with an xml file >^_^<"
+  raise SystemExit
+elif fnmatch.fnmatch(sys.argv[1], "*.xml"): 
+  filename = sys.argv[1]
+  tree = ET.parse(codecs.open(filename, encoding = "UTF-8")) # build tree
+  smellingBerries(tree)
+else:
+  print "Phrasel Ferret farts in your general direction"
 
 # test structure 
-for child in root:
-  print child.tag, child.attrib
+def smellingBerries(ET):
+  root = tree.getroot()
+  allChars = root.findall(".//*[@font]")
+  for char in allChars:
+    print char.text
+
+print "done?"
+
+raise SystemExit
 
