84534a09423eabcf173349777ec97d471fe4e9bf
Andrew Kampjes <a.kampjes@gmail.com>
2014-01-28 11:04:34 +1300

    Made Ferret skip characters it doesn't understand
    Had to change to python3 to make even the cv.xml file work.
    I had to move smellingBerries() to the top, you can't use functions until they
      have been declared.
    Switched to LXML which has additional features, most importantly, you can tell
    it to continue on errors with recover=True.
    Also, python style guideline http://www.python.org/dev/peps/pep-0008/ not that
      it's a big deal, just function naming style.

--- FirstFerret.py
+++ FirstFerret.py
@@ -1,33 +1,36 @@
-#! /usr/bin/python
+#! /usr/bin/python3
 
 import codecs
-import xml.etree.ElementTree as ET
+#import xml.etree.ElementTree as ET
+from lxml import etree  # Lets use this as the xml parser, it has some more features :D
 import sys
 import fnmatch
 
+
+# Phrasal Ferret needs to find some berries to eat. Will he find anything?
+# He looks through the leaves (elements) to see what he can find
+def smellingBerries():
+  root = tree.getroot()
+  allChars = root.findall(".//*[@font]")
+  for char in allChars:
+    print(char.text)
+
+
 # Read in the file name
 if len(sys.argv) != 2:
-  print "Usage: FirstFerret.py <xml file> \nPhrasel Ferret likes to climb xml trees. They have the softest leaves and pleasant smelling flowers. Please try again with an xml file >^_^<"
+  print("Usage: FirstFerret.py <xml file> \nPhrasel Ferret likes to climb xml trees. They have the softest leaves and pleasant smelling flowers. Please try again with an xml file >^_^<")
   raise SystemExit
 elif fnmatch.fnmatch(sys.argv[1], "*.xml"): 
   filename = sys.argv[1]
   try:
-    tree = ET.parse(codecs.open(filename, encoding = "UTF-8", errors='replace')) # build tree. This is where the error is occuring!
+    parser = etree.XMLParser(recover=True)
+    tree = etree.parse(codecs.open(filename, encoding = "UTF-8", errors='replace'), parser=parser) # build tree. This is where the error is occuring!
   except:
-    print "OH NO. Phrasal Ferret fell from the branch. PARSING ERROR (probably)"
-  smellingBerries(tree)
+    print("OH NO. Phrasal Ferret fell from the branch. (Looks like something went wrong)") 
+  smellingBerries()
 else:
-  print "Phrasel Ferret farts in your general direction."
+  print("Phrasel Ferret farts in your general direction.")
 
-# Phrasal Ferret needs to find some berries to eat. Will he find anything?
-# He looks through the leaves (elements) to see what he can find
-def smellingBerries(ET):
-  root = tree.getroot()
-  allChars = root.findall(".//*[@font]")
-  for char in allChars:
-    print char.text
-
-print "done?"
-
+print("done?")
 raise SystemExit
 
