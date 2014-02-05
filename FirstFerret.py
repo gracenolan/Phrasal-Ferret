d7a5d14a78c650e1f2eec44bc929855086f7b2af
Grace Nolan <grace.m.nolan@gmail.com>
2014-02-05 14:48:15 +1300

    can't find the end of phrases
    I’m creating phrases and they are getting discarded before they are
    saved into an array

--- FirstFerret.py
+++ FirstFerret.py
@@ -6,6 +6,9 @@ from lxml import etree  # Lets use this as the xml parser, it has some more feat
 import sys
 import fnmatch
 
+# This is Phrasal Ferret's collection of phrases 
+# dictionary of phrases with a count of how often they appear 
+phrases = {}
 
 # Phrasal Ferret needs to find some berries to eat. Will he find anything?
 # He looks through the leaves (elements) to see what he can find
@@ -17,17 +20,44 @@ def print_all():
 
 #bold_phrase = ""
 # method to find teh bold characters
+font = "" # this holds the font tag so changes in font are detected
 def find_bold(textline):
-  bold_phrase = "" 
+  bold_phrase = ""
+  global font
   for text in textline:
     if not text.tag == "text":
       continue
     #print(text.text),
     if text.get("font"):
-      if "Bold" in text.get("font"):
-        bold_phrase += text.text # this doesn't solve the split line problem
-        print(text.text),
-  
+      if font == text.get("font"):  
+        # Is this character part of a phrase we are already recording?
+        # if this character is the same font as the last character
+        # then append it to the current bold_phrase string
+        bold_phrase += text.text 
+        print(bold_phrase + "....")
+
+      # if it is not the same, then see if the font is bold
+      elif "Bold" in text.get("font"):
+        # if this character is bold (the beginning of a phrase)
+        # we need to save the old phrase
+        # then add it to the bold_phrase. 
+        # this will be the start of a new bold phrase
+        # bold_phrase = text.text
+        if len(bold_phrase) > 2: #if the phrase is actually holding something
+          if not bold_phrase in phrases:
+            # save last word
+            print("saving ne word to phrases{}")
+            phrases[bold_phrase] = 1
+          else:
+            print("incrementing " + bold_phrase + "in phrases{}")
+            phrases[bold_phrase] += 1
+
+        bold_phrase = text.text     # clear phrase and save beginning of new phrase
+        font = text.get("font")
+        print("Bold: " + text.text),
+    # else try find italics ?
+      else:
+        continue
 
 # Phrasal Ferret excitedly wiggles it's nose! It scuttles along the branches 
 # in search for tasty text he can analyse. 
@@ -68,11 +98,11 @@ elif fnmatch.fnmatch(sys.argv[1], "*.xml"):
     tree = etree.parse(codecs.open(filename, encoding = "UTF-8", errors='replace'), parser=parser) # build tree. This is where the error is occuring!
   except:
     print("OH NO. Phrasal Ferret fell from the branch. (Looks like something went wrong)") 
-  print_all()
   find_text()
 else:
   print("Phrasel Ferret farts in your general direction.")
 
+print(phrases)
 print("done")
 raise SystemExit
 
